2859e1aed605cd411f1a99eaabc989aef70ac578
cnweibo <matiascx@163.com>
2016-01-21 21:08:00 +0800

    getOwnPropertyNames and getOwnFunctionNames wrapper

--- jshelper.js
+++ jshelper.js
@@ -7,3 +7,31 @@
 function getType(obj) {
     return obj && obj.constructor && obj.constructor.toString().match(/function\s*([^(]*)/)[1];
 }
+/**
+ * retrieve the own property names for given object
+ * @param  {Object} obj the object to be inspected
+ * @return {Array}     Array with string of own property names for the obj
+ */
+function getOwnPropertyNames(obj) {
+    return Object.getOwnPropertyNames(obj); 
+}
+/**
+ * retrieve the given object's own function names
+ * @param  {Object} obj object which is to be inspected
+ * @return {Array}     array filled with obj's own function names;
+ *                     the arguments/caller are skipped due to strict mode exception
+ */
+function getOwnFunctionNames(obj) {
+    var properties = Object.getOwnPropertyNames(obj);
+    var length = properties.length;
+    var functions = [];
+
+    for (var i = 0; i < length; i++) {
+        var property = properties[i];
+        if (property == "arguments" || property == "caller" ){continue;}
+        if (typeof obj[property] === "function"){
+            functions.push(property); 
+        }
+    }
+    return functions;
+}
