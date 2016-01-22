f55d58abe5cc5fb4c9140adf5e7864e914c2cf98
cnweibo <matiascx@163.com>
2016-01-23 00:25:39 +0800

    getType returns unified lowercase type string

--- jshelper.js
+++ jshelper.js
@@ -2,10 +2,10 @@
  * retrive the parameter object's type via obj.constructor
  * with small hack with obj && obj.constructor prefixed
  * @param  {Object or undefined or null} obj object to be type checked
- * @return {[type]}     constructor function name or null/undefined
+ * @return {string} lowercase constructor function name or null/undefined
  */
 function getType(obj) {
-    return obj && obj.constructor && obj.constructor.toString().match(/function\s*([^(]*)/)[1];
+    return obj && obj.constructor && obj.constructor.toString().match(/function\s*([^(]*)/)[1].toLowerCase();
 }
 /**
  * retrieve the own property names for given object
